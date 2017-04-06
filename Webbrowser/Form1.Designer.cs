db0ed82026c0ab6c69e33c326e60277272ef0e39
маруська <@->
2017-04-06 14:32:00 +0300

    Add project files.

---
+++ Webbrowser/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace Webbrowser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnBack = new System.Windows.Forms.Button();
+            this.btnForward = new System.Windows.Forms.Button();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.btnNewTab = new System.Windows.Forms.Button();
+            this.textUrl = new System.Windows.Forms.TextBox();
+            this.tabControl = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.webBrowser = new System.Windows.Forms.WebBrowser();
+            this.tabControl.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnBack
+            // 
+            resources.ApplyResources(this.btnBack, "btnBack");
+            this.btnBack.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnBack.Name = "btnBack";
+            this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.UseWaitCursor = true;
+            this.btnBack.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnForward
+            // 
+            resources.ApplyResources(this.btnForward, "btnForward");
+            this.btnForward.Name = "btnForward";
+            this.btnForward.UseVisualStyleBackColor = true;
+            this.btnForward.UseWaitCursor = true;
+            this.btnForward.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnGo
+            // 
+            resources.ApplyResources(this.btnGo, "btnGo");
+            this.btnGo.Name = "btnGo";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.UseWaitCursor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // btnNewTab
+            // 
+            resources.ApplyResources(this.btnNewTab, "btnNewTab");
+            this.btnNewTab.Name = "btnNewTab";
+            this.btnNewTab.UseVisualStyleBackColor = true;
+            this.btnNewTab.UseWaitCursor = true;
+            this.btnNewTab.Click += new System.EventHandler(this.btnNewTab_Click);
+            // 
+            // textUrl
+            // 
+            resources.ApplyResources(this.textUrl, "textUrl");
+            this.textUrl.Name = "textUrl";
+            this.textUrl.UseWaitCursor = true;
+            this.textUrl.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textUrl_KeyPress);
+            // 
+            // tabControl
+            // 
+            resources.ApplyResources(this.tabControl, "tabControl");
+            this.tabControl.Controls.Add(this.tabPage1);
+            this.tabControl.Name = "tabControl";
+            this.tabControl.SelectedIndex = 0;
+            this.tabControl.UseWaitCursor = true;
+            // 
+            // tabPage1
+            // 
+            resources.ApplyResources(this.tabPage1, "tabPage1");
+            this.tabPage1.Controls.Add(this.webBrowser);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            this.tabPage1.UseWaitCursor = true;
+            // 
+            // webBrowser
+            // 
+            resources.ApplyResources(this.webBrowser, "webBrowser");
+            this.webBrowser.Name = "webBrowser";
+            this.webBrowser.ScriptErrorsSuppressed = true;
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.BackColor = System.Drawing.Color.Linen;
+            this.Controls.Add(this.tabControl);
+            this.Controls.Add(this.textUrl);
+            this.Controls.Add(this.btnNewTab);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.btnForward);
+            this.Controls.Add(this.btnBack);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.IsMdiContainer = true;
+            this.Name = "Form1";
+            this.TransparencyKey = System.Drawing.Color.DarkGreen;
+            this.UseWaitCursor = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnForward;
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.Button btnNewTab;
+        private System.Windows.Forms.TextBox textUrl;
+        private System.Windows.Forms.TabControl tabControl;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.WebBrowser webBrowser;
+        public System.Windows.Forms.Button btnBack;
+    }
+}
+
---
+++ Webbrowser/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Webbrowser
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            WebBrowser web = tabControl.SelectedTab.Controls[0] as WebBrowser;
+            if (web != null)
+            {
+                if (web.CanGoBack)
+                    web.GoBack();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            WebBrowser web = tabControl.SelectedTab.Controls[0] as WebBrowser;
+            if (web != null)
+            {
+                if (web.CanGoForward)
+                    web.GoForward();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            webBrowser.Navigate("https://www.google.com.ua");
+            webBrowser.DocumentCompleted += WebBrowser_DocumentComplited;
+        }
+
+        private void WebBrowser_DocumentComplited(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            tabControl.SelectedTab.Text= webBrowser.DocumentTitle;
+
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            WebBrowser web = tabControl.SelectedTab.Controls[0] as WebBrowser;
+            if (web != null)
+                web.Navigate(textUrl.Text);
+        }
+
+        private WebBrowser webTab = null;
+        private void btnNewTab_Click(object sender, EventArgs e)
+        {
+            TabPage tab = new TabPage();
+            tab.Text = "New Tab";
+            tabControl.Controls.Add(tab);
+            tabControl.SelectTab(tabControl.TabCount - 1);
+            webTab = new WebBrowser() { ScriptErrorsSuppressed = true};
+            webTab.Parent = tab;
+            webTab.Navigate("https://www.google.com.ua");
+            textUrl.Text = "https://www.google.com.ua";
+            webTab.Dock = DockStyle.Fill;
+            webTab.DocumentCompleted += WebTab_DocumentCompleted;
+            
+        }
+
+        private void WebTab_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            tabControl.SelectedTab.Text = webTab.DocumentTitle;
+
+        }
+
+        private void textUrl_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+            if (e.KeyChar == (char)13)
+            {
+                WebBrowser web = tabControl.SelectedTab.Controls[0] as WebBrowser;
+                if (web != null)
+                    web.Navigate(textUrl.Text);
+            }
+
+        }
+    }
+
+
+}
